6ae3932549bbaf9c2ab4c1d4bba861ca4abe1fd4
itzSumma <itzSumma11@gmail.com>
2026-03-11 08:35:26 +0600

    add cart in storage

--- Browser-Storage/Shopping-Cart/script.js
+++ Browser-Storage/Shopping-Cart/script.js
@@ -7,12 +7,25 @@ const productElement =document.getElementById("product");
 const quantityElement = document.getElementById("quantity");
 const product =productElement.value;
 const quantity = quantityElement.value;
-console.log("add product",product,quantity)
+// console.log("add product",product,quantity)
 
 showProduct(product,quantity);
 
+addPoductToCart(product,quantity)
+
 productElement.value="";
 quantityElement.value="";
+}
+
+//empty cart
+const getCart = ()=>{
+    const cart = {};
+    return cart;
+}
+const addPoductToCart =(product,quantity)=>{
+    const cart = getCart();
+    cart[product] = quantity;
+    console.log("cart", cart)
 }
 
 const showProduct =(product,quantity)=>{
@@ -23,3 +36,9 @@ const ul = document.getElementById( "products-container");
 ul.appendChild(li)
 
 }
+
+/**
+ * 1.To save object/array in the LS 
+ * 2.Convert the object to JSON  string by using JSON.stringify
+ * 3.localStorage.setItem()
+ */
