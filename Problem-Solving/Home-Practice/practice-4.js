91daf05999d26938b36f245b8861435d20c9f76e
itzSumma <itzSumma11@gmail.com>
2026-03-16 17:33:14 +0600

    remove falsy value from array

--- Problem-Solving/Home-Practice/practice-4.js
+++ Problem-Solving/Home-Practice/practice-4.js
@@ -14,3 +14,20 @@ console.log(isPalindrome("madam"));
 console.log(isPalindrome("hello")); 
 
 
+/**
+ * * Remove all falsy values from an array.
+**false, 0, "", null, undefined, NaN */  
+ function removeFalsy(arr) {
+  return arr.filter(Boolean);
+}
+
+console.log(removeFalsy([0, "hello", false, 42, "", null]));
+///2 ways
+const arr = [0, "hello", false, 42, "", null];
+
+const result = arr.filter(Boolean);
+
+console.log(result);
+
+
+
