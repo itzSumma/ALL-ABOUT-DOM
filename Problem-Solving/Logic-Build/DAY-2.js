7f4bff13ba201975aec8d51474281a2e7be3049f
itzSumma <itzSumma11@gmail.com>
2026-03-21 11:29:59 +0600

    Add function to count character frequency in a string

--- Problem-Solving/Logic-Build/DAY-2.js
+++ Problem-Solving/Logic-Build/DAY-2.js
@@ -20,3 +20,16 @@ function longestSubstring(str) {
   return maxLength;
 }
 console.log(longestSubstring("abcabcbb"));
+
+/**
+ * *Count how many times each character appears in a string
+ */
+function charCount(str) {
+  let count = {};
+  for (let char of str) {
+    count[char] = (count[char] || 0) + 1;
+  }
+  return count;
+}
+
+console.log(charCount("aabbc"));
