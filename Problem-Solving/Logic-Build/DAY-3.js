c49b7190b07569dc6570fa1025abdc3287f017b2
itzSumma <itzSumma11@gmail.com>
2026-05-10 20:10:04 +0600

    print number from 1-20

--- Problem-Solving/Logic-Build/DAY-3.js
+++ Problem-Solving/Logic-Build/DAY-3.js
@@ -33,3 +33,23 @@ function isPrime(num) {
 }
 
 console.log(isPrime(7));
+
+/*
+ * Print numbers from 1 to 20.
+ */
+
+function fizzBuzz() {
+  for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+fizzBuzz();
