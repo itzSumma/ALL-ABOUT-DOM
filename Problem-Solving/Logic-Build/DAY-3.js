bbbbf424329b44c1b01983df9dd5847fa6c8509c
itzSumma <itzSumma11@gmail.com>
2026-05-10 20:11:17 +0600

    find the missing number from array

--- Problem-Solving/Logic-Build/DAY-3.js
+++ Problem-Solving/Logic-Build/DAY-3.js
@@ -53,3 +53,22 @@ function fizzBuzz() {
 }
 
 fizzBuzz();
+
+/*
+ * Find the missing number from an array.
+ */
+function findMissingNumber(arr) {
+  let n = arr.length + 1;
+
+  let expectedSum = (n * (n + 1)) / 2;
+
+  let actualSum = 0;
+
+  for (let num of arr) {
+    actualSum += num;
+  }
+
+  return expectedSum - actualSum;
+}
+
+console.log(findMissingNumber([1, 2, 3, 5]));
