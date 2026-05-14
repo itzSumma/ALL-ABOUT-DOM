d339a13e033d705061175fc0618862c6b3ee5921
itzSumma <itzSumma11@gmail.com>
2026-05-14 21:02:09 +0600

    feat: add logic to group students by grades based on scores

--- Problem-Solving/Home-Practice/Intermediete/Part-1.js
+++ Problem-Solving/Home-Practice/Intermediete/Part-1.js
@@ -48,4 +48,45 @@ const groupedProducts = products.reduce((accumulator, product) => {
   return accumulator;
 }, {}); 
 
-console.log(groupedProducts);
+// console.log(groupedProducts);
+
+
+/*
+ * You are given an array of student objects. Each student has a name and a score. Your task is to group these students into three grades based on their scores:
+*Grade A: Score 90 or above.
+*Grade B: Score 80 to 89.
+*Grade C: Score below 80.
+ */
+Input :const students = [
+  { name: "Rahim", score: 95 },
+  { name: "Karim", score: 82 },
+  { name: "Sara", score: 75 },
+  { name: "John", score: 91 },
+  { name: "Tina", score: 88 }
+];
+//**ANS*/
+const students = [
+  { name: "Rahim", score: 95 },
+  { name: "Karim", score: 82 },
+  { name: "Sara", score: 75 },
+  { name: "John", score: 91 },
+  { name: "Tina", score: 88 }
+];
+
+const gradedStudents = students.reduce((acc, student) => {
+  let grade;
+  if (student.score >= 90) {
+    grade = "GradeA";
+  } else if (student.score >= 80) {
+    grade = "GradeB";
+  } else {
+    grade = "GradeC";
+  }
+  if (!acc[grade]) {
+    acc[grade] = []; 
+  }
+  acc[grade].push(student);
+  return acc;
+}, {});
+
+// console.log(gradedStudents);
