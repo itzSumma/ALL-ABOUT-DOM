bb6b86ef92e3eab96afe6b4c33eb23c1b14d1ce6
itzSumma <itzSumma11@gmail.com>
2026-05-27 11:42:59 +0600

    feat: add optimized array product solution

--- Problem-Solving/Home-Practice/Intermediete/Part-3.js
+++ Problem-Solving/Home-Practice/Intermediete/Part-3.js
@@ -29,3 +29,27 @@ function lengthOfLongestSubstring(s) {
 }
 
 console.log(lengthOfLongestSubstring("abcabcbb"));
+
+//*Given an integer array nums, return an array answer such that:answer[i] = product of all elements except nums[i]
+  //*Answer:*/
+function productExceptSelf(nums) {
+  let result = new Array(nums.length).fill(1);
+
+  let prefix = 1;
+
+  for (let i = 0; i < nums.length; i++) {
+    result[i] = prefix;
+    prefix *= nums[i];
+  }
+
+  let postfix = 1;
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    result[i] *= postfix;
+    postfix *= nums[i];
+  }
+
+  return result;
+}
+
+console.log(productExceptSelf([1,2,3,4]));
