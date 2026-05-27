e825e2edb54f1af862b28127e263c44aca9fc1ca
itzSumma <itzSumma11@gmail.com>
2026-05-27 11:41:01 +0600

    feat: add optimized O(n) longest substring algorithm

---
+++ Problem-Solving/Home-Practice/Intermediete/Part-3.js
@@ -0,0 +1,31 @@
+/*
+ *Given a string s, find the length of the longest substring without repeating characters. 
+ */
+
+ Input: "abcabcbb"
+Output: 3
+
+/**
+ * *ANS*/
+function lengthOfLongestSubstring(s) {
+  let set = new Set();
+
+  let left = 0;
+  let maxLength = 0;
+
+  for (let right = 0; right < s.length; right++) {
+
+    while (set.has(s[right])) {
+      set.delete(s[left]);
+      left++;
+    }
+
+    set.add(s[right]);
+
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+
+  return maxLength;
+}
+
+console.log(lengthOfLongestSubstring("abcabcbb"));
